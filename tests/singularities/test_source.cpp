47002b9941c0d7f31b25b4914b14a40d14ac4082
Daniel Prosser <dpross1100@msn.com>
2018-04-07 14:59:48 -0400

    Add source test with rotation and translation

--- tests/singularities/test_source.cpp
+++ tests/singularities/test_source.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include "singularities.h"
 #include "quadpanel.h"
+#include "tripanel.h"
 #include "vertex.h"
 #include "settings.h"
 
@@ -12,11 +13,12 @@ int main ()
 {
   Vertex v1, v2, v3, v4;
   QuadPanel quadpanel;
+  TriPanel tri1, tri2;
   std::vector<std::vector<Vertex> > point_sources;
-  std::vector<double> y;
-  std::vector<Eigen::Vector3d> velarr, velq;
+  std::vector<double> x;
+  std::vector<Eigen::Vector3d> velarr, velq, velt;
   unsigned int npoints, i, j, k, nx, ny;
-  double deltx, delty, psource, px, py, x, z, ymin, ymax, dy;
+  double deltx, delty, psource, px, py, y, z, xmin, xmax, dx;
   std::ofstream f;
 
   farfield_distance_factor = 1.E+06;
@@ -32,6 +34,18 @@ int main ()
   quadpanel.addVertex(&v1);
   quadpanel.setSourceStrength(1.0);
   quadpanel.setDoubletStrength(0.0);
+
+  tri1.addVertex(&v4);
+  tri1.addVertex(&v3);
+  tri1.addVertex(&v2);
+  tri1.setSourceStrength(1.0);
+  tri1.setDoubletStrength(0.0);
+
+  tri2.addVertex(&v2);
+  tri2.addVertex(&v1);
+  tri2.addVertex(&v4);
+  tri2.setSourceStrength(1.0);
+  tri2.setDoubletStrength(0.0);
 
   // Create an array of point sources to compare with quad panel
 
@@ -53,17 +67,18 @@ int main ()
   }
 
   npoints = 100;
-  ymin = -5;
-  ymax = 5;
-  dy = (ymax-ymin)/double(npoints-1);
-  y.resize(npoints);
+  xmin = -5;
+  xmax = 5;
+  dx = (xmax-xmin)/double(npoints-1);
+  x.resize(npoints);
   velarr.resize(npoints);
   velq.resize(npoints);
-  x = 0.5;
-  z = 1.0;
+  velt.resize(npoints);
+  y = 0.5;
+  z = -1.0;
   for ( i = 0; i < npoints; i++ )
   {
-    y[i] = ymin + double(i)*dy;
+    x[i] = xmin + double(i)*dx;
     velarr[i](0) = 0.;
     velarr[i](1) = 0.;
     velarr[i](2) = 0.;
@@ -71,30 +86,43 @@ int main ()
     {
       for ( k = 0; k < ny; k++ )
       {
-        velarr[i] += point_source_velocity(x-point_sources[j][k].x(),
-                                           y[i]-point_sources[j][k].y(),
+        velarr[i] += point_source_velocity(x[i]-point_sources[j][k].x(),
+                                           y-point_sources[j][k].y(),
                                            z-point_sources[j][k].z())*psource;
       }
     }
-    velq[i] = quadpanel.inducedVelocity(x, y[i], z, false, "top");
+    velq[i] = quadpanel.inducedVelocity(x[i], y, z, false, "top");
+    velt[i] = tri1.inducedVelocity(x[i], y, z, false, "top")
+            + tri2.inducedVelocity(x[i], y, z, false, "top");
   } 
 
   f.open("source1.dat");
   f << "VARIABLES = \"x\" \"y\" \"z\" \"Vx\" \"Vy\" \"Vz\"" << std::endl;
   for ( i = 0; i < npoints; i++ )
   {
-    f << std::setprecision(9) << x << " " << y[i] << " " << z << " "
+    f << std::setprecision(9) << x[i] << " " << y << " " << z << " "
       << velq[i](0) << " " << velq[i](1) << " " << velq[i](2) << " "
       << std::endl;
   }
   f.close();
   std::cout << "Wrote file source1.dat." << std::endl;
 
+  f.open("source2.dat");
+  f << "VARIABLES = \"x\" \"y\" \"z\" \"Vx\" \"Vy\" \"Vz\"" << std::endl;
+  for ( i = 0; i < npoints; i++ )
+  {
+    f << std::setprecision(9) << x[i] << " " << y << " " << z << " "
+      << velt[i](0) << " " << velt[i](1) << " " << velt[i](2) << " "
+      << std::endl;
+  }
+  f.close();
+  std::cout << "Wrote file source2.dat." << std::endl;
+
   f.open("sourcearr1.dat");
   f << "VARIABLES = \"x\" \"y\" \"z\" \"Vx\" \"Vy\" \"Vz\"" << std::endl;
   for ( i = 0; i < npoints; i++ )
   {
-    f << std::setprecision(9) << x << " " << y[i] << " " << z << " "
+    f << std::setprecision(9) << x[i] << " " << y << " " << z << " "
       << velarr[i](0) << " " << velarr[i](1) << " " << velarr[i](2) << " "
       << std::endl;
   }
--- tests/singularities/test_source.py
+++ tests/singularities/test_source.py
@@ -3,35 +3,51 @@
 from data_loader import load_data as ld
 from matplotlib import pyplot as plt
 
-def source_vs_sourcearr(source, sourcearr):
+def source_vs_sourcearr(source, source2, sourcearr):
+
+  # Plot x velocity
+  plt.clf()
+  plt.close()
+  fig, ax = plt.subplots()
+  ax.set_xlabel('x')
+  ax.set_ylabel('Vx')
+  ax.plot(source[:,0], source[:,3], '-x')
+  ax.plot(source2[:,0], source2[:,3], '-+', markersize=10)
+  ax.plot(sourcearr[:,0], sourcearr[:,3])
+  ax.grid()
+  ax.legend(['Quad source', 'Tri sources', 'Source array'])
+  plt.show()
 
   # Plot y velocity
   plt.clf()
   plt.close()
   fig, ax = plt.subplots()
-  ax.set_xlabel('y')
+  ax.set_xlabel('x')
   ax.set_ylabel('Vy')
-  ax.plot(source[:,1], source[:,4], '-x')
-  ax.plot(sourcearr[:,1], sourcearr[:,4])
+  ax.plot(source[:,0], source[:,4], '-x')
+  ax.plot(source2[:,0], source2[:,4], '-+', markersize=10)
+  ax.plot(sourcearr[:,0], sourcearr[:,4])
   ax.grid()
-  ax.legend(['Quad source', 'Source array'])
+  ax.legend(['Quad source', 'Tri sources', 'Source array'])
   plt.show()
 
   # Plot z velocity
   plt.clf()
   plt.close()
   fig, ax = plt.subplots()
-  ax.set_xlabel('y')
+  ax.set_xlabel('x')
   ax.set_ylabel('Vz')
-  ax.plot(source[:,1], source[:,5], '-x')
-  ax.plot(sourcearr[:,1], sourcearr[:,5])
+  ax.plot(source[:,0], source[:,5], '-x')
+  ax.plot(source2[:,0], source2[:,5], '-+', markersize=10)
+  ax.plot(sourcearr[:,0], sourcearr[:,5])
   ax.grid()
-  ax.legend(['Quad source', 'Source array'])
+  ax.legend(['Quad source', 'Tri sources', 'Source array'])
   plt.show()
 
 if __name__ == "__main__":
 
   source = ld('source1.dat')
+  source2 = ld('source2.dat')
   sourcearr = ld('sourcearr1.dat')
 
-  source_vs_sourcearr(source, sourcearr)
+  source_vs_sourcearr(source, source2, sourcearr)
