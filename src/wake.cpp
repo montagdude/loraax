1f3e508785583f1eea8a9e1b7c30b29275ccbde1
Daniel Prosser <dpross1100@msn.com>
2018-06-28 23:49:12 -0400

    Compute Trefftz plane velocities at edge midpoints
    Makes negligible difference to forces, but simplifies calculation

--- src/wake.cpp
+++ src/wake.cpp
@@ -454,16 +454,19 @@ void Wake::farfieldForces ( const double & xtrefftz, const double & ztrefftz,
 
 	// Wake induced velocity
 
-	w.resize(_nspan);
+	w.resize(_nspan-1);
 	nwake = allwake.size();
 #pragma omp parallel for private(i,p0,p,j)
-	for ( i = 0; i < _nspan; i++ )
+	for ( i = 0; i < _nspan-1; i++ )
 	{
-		// Aft TE point projected on Trefftz plane
+		// Aft TE midpoint projected on Trefftz plane
 
-		p0(0) = _verts[i*(_nstream+1)+0].x();
-		p0(1) = _verts[i*(_nstream+1)+0].y();
-		p0(2) = _verts[i*(_nstream+1)+0].z();
+		p0(0) = 0.5*(_verts[    i*(_nstream+1)+0].x()
+		      +      _verts[(i+1)*(_nstream+1)+0].x());
+		p0(1) = 0.5*(_verts[    i*(_nstream+1)+0].y()
+		      +      _verts[(i+1)*(_nstream+1)+0].y());
+		p0(2) = 0.5*(_verts[    i*(_nstream+1)+0].z()
+		      +      _verts[(i+1)*(_nstream+1)+0].z());
 		p = line_plane_intersection(p0, uinfvec/uinf, a, b, c, d);
 
 		// Induced velocity from all wakes and transform to Trefftz plane
@@ -471,12 +474,8 @@ void Wake::farfieldForces ( const double & xtrefftz, const double & ztrefftz,
 		w[i] << 0., 0., 0.;
 		for ( j = 0; j < nwake; j++ )
 		{
-			if (allwake[j]->idx() == _idx)
-				w[i] += allwake[j]->planarInducedVelocity(p(0), p(1), p(2),
-				                                          false, i);
-			else
-				w[i] += allwake[j]->planarInducedVelocity(p(0), p(1), p(2),
-				                                          false);
+			w[i] += allwake[j]->planarInducedVelocity(p(0), p(1), p(2),
+			                                          false);
 		}
 		w[i] = transform*w[i];
 	}
@@ -507,8 +506,8 @@ void Wake::farfieldForces ( const double & xtrefftz, const double & ztrefftz,
 
 		// In-plane velocity components
 
-		wy = 0.5*(w[i](1) + w[i+1](1));
-		wz = 0.5*(w[i](2) + w[i+1](2));
+		wy = w[i](1);
+		wz = w[i](2);
 
 		// Lift and induced drag
 
