97bd05ef439857f79e9ff32ee8c3e53125734c31
Daniel Prosser <dpross1100@msn.com>
2019-01-13 12:59:07 -0500

    Some more tabs to spaces

--- src/settings.cpp
+++ src/settings.cpp
@@ -48,65 +48,69 @@ xfoil_options_type xfoil_run_opts;
 int read_setting ( const XMLElement *elem, const std::string & setting,
                    std::string & value, bool required )
 {
-  if (elem->FirstChildElement(setting.c_str()))
-    value = elem->FirstChildElement(setting.c_str())->GetText();
-  else
-  {
-    if (required)
-      conditional_stop(1, "read_setting",
-                      "No " + setting +" in xml element " + elem->Name() + ".");
-    return 1;
-  }
-
-  return 0;
+    if (elem->FirstChildElement(setting.c_str()))
+        value = elem->FirstChildElement(setting.c_str())->GetText();
+    else
+    {
+        if (required)
+          conditional_stop(1, "read_setting",
+                          "No " + setting +" in xml element " + elem->Name() +
+                          ".");
+        return 1;
+    }
+  
+    return 0;
 }
 
 int read_setting ( const XMLElement *elem, const std::string & setting,
                    double & value, bool required )
 {
-  if (elem->FirstChildElement(setting.c_str()))
-    elem->FirstChildElement(setting.c_str())->QueryDoubleText(&value);
-  else
-  {
-    if (required)
-      conditional_stop(1, "read_setting",
-                      "No " + setting +" in xml element " + elem->Name() + ".");
-    return 1;
-  }
+    if (elem->FirstChildElement(setting.c_str()))
+        elem->FirstChildElement(setting.c_str())->QueryDoubleText(&value);
+    else
+    {
+        if (required)
+            conditional_stop(1, "read_setting",
+                            "No " + setting +" in xml element " + elem->Name() +
+                            ".");
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
 
 int read_setting ( const XMLElement *elem, const std::string & setting,
                    int & value, bool required )
 {
-  if (elem->FirstChildElement(setting.c_str()))
-    elem->FirstChildElement(setting.c_str())->QueryIntText(&value);
-  else
-  {
-    if (required)
-      conditional_stop(1, "read_setting",
-                      "No " + setting +" in xml element " + elem->Name() + ".");
-    return 1;
-  }
-
-  return 0;
+    if (elem->FirstChildElement(setting.c_str()))
+        elem->FirstChildElement(setting.c_str())->QueryIntText(&value);
+    else
+    {
+        if (required)
+            conditional_stop(1, "read_setting",
+                            "No " + setting +" in xml element " + elem->Name() +
+                            ".");
+        return 1;
+    }
+  
+    return 0;
 }
 
 int read_setting ( const XMLElement *elem, const std::string & setting,
                    bool & value, bool required )
 {
-  if (elem->FirstChildElement(setting.c_str()))
-    elem->FirstChildElement(setting.c_str())->QueryBoolText(&value);
-  else
-  {
-    if (required)
-      conditional_stop(1, "read_setting",
-                      "No " + setting +" in xml element " + elem->Name() + ".");
-    return 1;
-  }
-
-  return 0;
+    if (elem->FirstChildElement(setting.c_str()))
+        elem->FirstChildElement(setting.c_str())->QueryBoolText(&value);
+    else
+    {
+        if (required)
+            conditional_stop(1, "read_setting",
+                            "No " + setting +" in xml element " + elem->Name() +
+                            ".");
+        return 1;
+    }
+  
+    return 0;
 }
 
 /******************************************************************************/
@@ -116,118 +120,118 @@ int read_setting ( const XMLElement *elem, const std::string & setting,
 /******************************************************************************/
 int read_settings ( const std::string & inputfile, std::string & geom_file )
 {
-	XMLDocument doc;
+    XMLDocument doc;
 
-	doc.LoadFile(inputfile.c_str());
-	if ( (doc.ErrorID() == XML_ERROR_FILE_NOT_FOUND) ||
-	     (doc.ErrorID() == XML_ERROR_FILE_COULD_NOT_BE_OPENED) ||
-	     (doc.ErrorID() == XML_ERROR_FILE_READ_ERROR) )
-	{
-		conditional_stop(1, "read_settings",
-		                 "Could not read " + inputfile + ".");
-		return 1;
-	}
-	else if (doc.ErrorID() != 0)
-	{
-		conditional_stop(1, "read_settings",
-		                 "Syntax error in " + inputfile + ".");
-		return 1;
-	}
-	
-	XMLElement *main = doc.FirstChildElement("Main");
-	if (! main)
-	{
-		conditional_stop(1, "read_settings",
-		                 "Expected 'Main' element in input file."); 
-		return 2;
-	}
-	if (read_setting(main, "CaseName", casename) != 0)
-		return 2;
-	if (read_setting(main, "GeometryFile", geom_file) != 0)
-		return 2;
-	if (read_setting(main, "FreestreamSpeed", uinf) != 0)
-		return 2;
-	if (read_setting(main, "FreestreamStaticPressure", pinf) != 0)
-		return 2;
-	if (read_setting(main, "FreestreamDensity", rhoinf) != 0)
-		return 2;
-	if (read_setting(main, "FreestreamViscosity", muinf) != 0)
-		return 2;
-	if (read_setting(main, "AngleOfAttack", alpha) != 0)
-		return 2;
-	if (read_setting(main, "RollupWake", rollup_wake, false) != 0)
-		rollup_wake = false;
-	wakeangle = alpha;
-	if (rollup_wake)
-	{
-		if (read_setting(main, "RollupDist", rollupdist) != 0)
-			return 2;
-		if (read_setting(main, "WakeIters", wakeiters) != 0)
-			return 2;
-		read_setting(main, "InitialWakeAngle", wakeangle, false);
-	}
-	else
-	{
-		rollupdist = -1.;	// Will get set to max span later
-		wakeiters = 1;
-	}
-	if (read_setting(main, "Viscous", viscous) != 0)
-		return 2;
-	if (read_setting(main, "StoppingTolerance", stop_tol, false) != 0)
-		stop_tol = 1.E-5;
-	if (read_setting(main, "MaxIters", maxiters, false) != 0)
-		maxiters = 100;
-	if (read_setting(main, "MinIters", miniters, false) != 0)
-		miniters = 0;
-	if (read_setting(main, "VisualizationFrequency", viz_freq, false) != 0)
-		viz_freq = 1;
-	
-	xfoil_run_opts.ncrit = 9.;
-	xfoil_run_opts.xtript = 1.0;
-	xfoil_run_opts.xtripb = 1.0;
-	xfoil_run_opts.maxit = 100;
-	xfoil_run_opts.vaccel = 0.01;
-	reinit_freq = 5;
-	XMLElement *xfrun = main->FirstChildElement("XfoilRunOptions");
-	if (xfrun)
-	{
-		read_setting(xfrun, "ncrit", xfoil_run_opts.ncrit, false);
-		read_setting(xfrun, "xtript", xfoil_run_opts.xtript, false);
-		read_setting(xfrun, "xtripb", xfoil_run_opts.xtripb, false);
-		read_setting(xfrun, "maxit", xfoil_run_opts.maxit, false);
-		read_setting(xfrun, "vaccel", xfoil_run_opts.vaccel, false);
-		read_setting(xfrun, "reinit_freq", reinit_freq, false);
-	}
-	xfoil_run_opts.viscous_mode = viscous;
-	xfoil_run_opts.silent_mode = true;
-	
-	xfoil_geom_opts.npan = 160.;
-	xfoil_geom_opts.cvpar = 1.0;
-	xfoil_geom_opts.cterat = 0.15;
-	XMLElement *xfgeom = main->FirstChildElement("XfoilPaneling");
-	if (xfgeom)
-	{
-		read_setting(xfgeom, "npan", xfoil_geom_opts.npan, false);
-		read_setting(xfgeom, "cvpar", xfoil_geom_opts.cvpar, false);
-		read_setting(xfgeom, "cterat", xfoil_geom_opts.cterat, false);
-	}
-	xfoil_geom_opts.ctrrat = 0.20;
-	xfoil_geom_opts.xsref1 = 1.0;
-	xfoil_geom_opts.xsref2 = 1.0;
-	xfoil_geom_opts.xpref1 = 1.0;
-	xfoil_geom_opts.xpref2 = 1.0;
-	
-	// Set freestream vector, mach number, and time step size
-	
-	uinfvec(0) = uinf*cos(alpha*M_PI/180.);
-	uinfvec(1) = 0.;
-	uinfvec(2) = uinf*sin(alpha*M_PI/180.);
-	minf = uinf / std::sqrt(1.4*pinf/rhoinf);
-	if (minf >= 1.)
-		conditional_stop(1, "read_settings",
-		                 "Mach number must be subsonic.");
-	if (rollup_wake)
-		dt = rollupdist / (uinf * double(wakeiters));
-	
-	return 0;
+    doc.LoadFile(inputfile.c_str());
+    if ( (doc.ErrorID() == XML_ERROR_FILE_NOT_FOUND) ||
+         (doc.ErrorID() == XML_ERROR_FILE_COULD_NOT_BE_OPENED) ||
+         (doc.ErrorID() == XML_ERROR_FILE_READ_ERROR) )
+    {
+        conditional_stop(1, "read_settings",
+                         "Could not read " + inputfile + ".");
+        return 1;
+    }
+    else if (doc.ErrorID() != 0)
+    {
+        conditional_stop(1, "read_settings",
+                         "Syntax error in " + inputfile + ".");
+        return 1;
+    }
+    
+    XMLElement *main = doc.FirstChildElement("Main");
+    if (! main)
+    {
+        conditional_stop(1, "read_settings",
+                         "Expected 'Main' element in input file."); 
+        return 2;
+    }
+    if (read_setting(main, "CaseName", casename) != 0)
+        return 2;
+    if (read_setting(main, "GeometryFile", geom_file) != 0)
+        return 2;
+    if (read_setting(main, "FreestreamSpeed", uinf) != 0)
+        return 2;
+    if (read_setting(main, "FreestreamStaticPressure", pinf) != 0)
+        return 2;
+    if (read_setting(main, "FreestreamDensity", rhoinf) != 0)
+        return 2;
+    if (read_setting(main, "FreestreamViscosity", muinf) != 0)
+        return 2;
+    if (read_setting(main, "AngleOfAttack", alpha) != 0)
+        return 2;
+    if (read_setting(main, "RollupWake", rollup_wake, false) != 0)
+        rollup_wake = false;
+    wakeangle = alpha;
+    if (rollup_wake)
+    {
+        if (read_setting(main, "RollupDist", rollupdist) != 0)
+            return 2;
+        if (read_setting(main, "WakeIters", wakeiters) != 0)
+            return 2;
+        read_setting(main, "InitialWakeAngle", wakeangle, false);
+    }
+    else
+    {
+        rollupdist = -1.;   // Will get set to max span later
+        wakeiters = 1;
+    }
+    if (read_setting(main, "Viscous", viscous) != 0)
+        return 2;
+    if (read_setting(main, "StoppingTolerance", stop_tol, false) != 0)
+        stop_tol = 1.E-5;
+    if (read_setting(main, "MaxIters", maxiters, false) != 0)
+        maxiters = 100;
+    if (read_setting(main, "MinIters", miniters, false) != 0)
+        miniters = 0;
+    if (read_setting(main, "VisualizationFrequency", viz_freq, false) != 0)
+        viz_freq = 1;
+    
+    xfoil_run_opts.ncrit = 9.;
+    xfoil_run_opts.xtript = 1.0;
+    xfoil_run_opts.xtripb = 1.0;
+    xfoil_run_opts.maxit = 100;
+    xfoil_run_opts.vaccel = 0.01;
+    reinit_freq = 5;
+    XMLElement *xfrun = main->FirstChildElement("XfoilRunOptions");
+    if (xfrun)
+    {
+        read_setting(xfrun, "ncrit", xfoil_run_opts.ncrit, false);
+        read_setting(xfrun, "xtript", xfoil_run_opts.xtript, false);
+        read_setting(xfrun, "xtripb", xfoil_run_opts.xtripb, false);
+        read_setting(xfrun, "maxit", xfoil_run_opts.maxit, false);
+        read_setting(xfrun, "vaccel", xfoil_run_opts.vaccel, false);
+        read_setting(xfrun, "reinit_freq", reinit_freq, false);
+    }
+    xfoil_run_opts.viscous_mode = viscous;
+    xfoil_run_opts.silent_mode = true;
+    
+    xfoil_geom_opts.npan = 160.;
+    xfoil_geom_opts.cvpar = 1.0;
+    xfoil_geom_opts.cterat = 0.15;
+    XMLElement *xfgeom = main->FirstChildElement("XfoilPaneling");
+    if (xfgeom)
+    {
+        read_setting(xfgeom, "npan", xfoil_geom_opts.npan, false);
+        read_setting(xfgeom, "cvpar", xfoil_geom_opts.cvpar, false);
+        read_setting(xfgeom, "cterat", xfoil_geom_opts.cterat, false);
+    }
+    xfoil_geom_opts.ctrrat = 0.20;
+    xfoil_geom_opts.xsref1 = 1.0;
+    xfoil_geom_opts.xsref2 = 1.0;
+    xfoil_geom_opts.xpref1 = 1.0;
+    xfoil_geom_opts.xpref2 = 1.0;
+    
+    // Set freestream vector, mach number, and time step size
+    
+    uinfvec(0) = uinf*cos(alpha*M_PI/180.);
+    uinfvec(1) = 0.;
+    uinfvec(2) = uinf*sin(alpha*M_PI/180.);
+    minf = uinf / std::sqrt(1.4*pinf/rhoinf);
+    if (minf >= 1.)
+        conditional_stop(1, "read_settings",
+                         "Mach number must be subsonic.");
+    if (rollup_wake)
+        dt = rollupdist / (uinf * double(wakeiters));
+    
+    return 0;
 }
